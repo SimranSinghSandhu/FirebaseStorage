6f230c203222d505d9a8c0562d28e20d4f7190f9
Simran <sam.sandhu131@gmail.com>
2020-06-04 17:27:59 +0530

    Adding GoogleSignIn Button and setting up Google Sign in functionality with authentication on Firebase.

--- FirebaseStorageApp/AppDelegate.swift
+++ FirebaseStorageApp/AppDelegate.swift
@@ -7,13 +7,47 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+   
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance()?.delegate = self
+        
         return true
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print("Error Signing in -", error.localizedDescription)
+        } else {
+            
+            guard let authentication = user.authentication else {return}
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { (result, error) in
+                if error != nil {
+                    print("Error Authenticating with Firebase -", error?.localizedDescription)
+                } else {
+                    print("User Signed In!")
+                }
+                
+            }
+            
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
--- FirebaseStorageApp/ViewController.swift
+++ FirebaseStorageApp/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    // Setting Up Google Button.
+    let googleSignInBtn: GIDSignInButton = {
+        let btn = GIDSignInButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(googleSignInBtn) // Adding Google SignIn Button inside the View.
+        settingUpConstraints() // Setting Constraints
+        
+        // presenting the view controller for Signing In 
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        // Automatically sign in the user.
+        restoreCredentials()
     }
 
-
+    // Setting all the constraints of the View.
+    private func settingUpConstraints() {
+        googleSignInBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        googleSignInBtn.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        googleSignInBtn.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        googleSignInBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+    }
+    
+    // Restore the Previous Sign in, If the user already exists.
+    private func restoreCredentials() {
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+        if Auth.auth().currentUser != nil {
+            print("Current User Exists!")
+        } else {
+            print("No User Found!")
+        }
+    }
 }
 
