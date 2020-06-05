5d6798ad909c804bc05b33bb9c9ebd61b207f9e0
Simran <sam.sandhu131@gmail.com>
2020-06-05 12:26:29 +0530

    showing Action Sheet with Action of PhotoLibrary, Camera and Remove Photo. Also checking if the Camera or PhotoLibrary is not available in the device then show error as alert.

--- FirebaseStorageApp/UploadImageViewController.swift
+++ FirebaseStorageApp/UploadImageViewController.swift
@@ -60,6 +60,7 @@ extension UploadImageViewController {
     // When ShowImageView is Tapped.
     @objc func ImageViewTappedHandle() {
         print("Image View is Pressed!")
+        settingActionSheet()
     }
     
     private func settingConstraints() {
@@ -74,5 +75,95 @@ extension UploadImageViewController {
         showImageView.widthAnchor.constraint(equalToConstant: width - spacing).isActive = true
         showImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
     }
+}
+
+extension UploadImageViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    private func settingActionSheet() {
+    
+        // Creating a Controller as an Action Sheet.
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // Creating Actions that we wanna show on the Alert Controller
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+            print("Photo Library is Selected.")
+            self.settingImagePicker(sourceType: .photoLibrary)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            print("Camera is Selected.")
+            self.settingImagePicker(sourceType: .camera)
+        }
+        
+        // Removes the Already Selected Image
+        let removePhotoAction = UIAlertAction(title: "Remove Photo", style: .destructive) { (action) in
+            print("Remove Selected Photo.")
+            self.showImageView.image = UIImage(named: "placeholderImage.jpg")
+        }
+        
+        // Cancels the ActionSheet
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("Cancel Action Sheet.")
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        // Adding Actions to the Controller
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(removePhotoAction)
+        alertController.addAction(cancelAction)
+        
+        // Presenting the Controller.
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func settingImagePicker(sourceType: UIImagePickerController.SourceType) {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            pickerController.sourceType = sourceType
+            print("Source Type is Aviable.")
+        } else {
+            print("Sorry This Feature does not exist in this Device.")
+            self.dismiss(animated: true, completion: nil)
+            
+            // if PhotoLibrary or Camera is not avaiable in the Device then Show Error.
+            showErrorAlert()
+        }
+        
+        DispatchQueue.main.async {
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    //
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage { // If allows Editing is True
+            print("Edited Image is Selected.")
+            showImageView.image = image
+        } else if let image = info[.originalImage] as? UIImage { // If allows Editing is False
+            print("Original Image is Selected.")
+            showImageView.image = image
+        }
+        // Dismissing the ViewController when the image is selected.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Show Error if Camera or Photolibrary is not avaiable in the device.
+    private func showErrorAlert() {
+        let alertController = UIAlertController(title: "Feature Not Found.", message: "Sorry, This feature is not available in this Device.", preferredStyle: .alert)
+        
+        let okayAction = UIAlertAction(title: "Okay", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alertController.addAction(okayAction)
+        
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
 }
